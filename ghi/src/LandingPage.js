69af0460063ca8a9c65a17e948f4455e1f5577f8
Brad Belcher <bradlis13@gmail.com>
2023-08-28 22:02:55 -0700

    top of page complete

--- ghi/src/LandingPage.js
+++ ghi/src/LandingPage.js
@@ -1,56 +1,53 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import "./LandingPage.css";
+import logo from "./media/logo.png";
 
-const LandingPage = () => {
+function LandingPage() {
   return (
-    <div className="landing-page">
-      <header>
-        <nav>
-          {/* Navigation Links */}
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/signup">Sign Up</NavLink>
-          <NavLink to="/login">Login</NavLink>
-        </nav>
-      </header>
-
-      <main>
-        <section className="intro-section">
-          <img src="path_to_logo_image" alt="Application Logo" />
-          <p>Your Application Description Here...</p>
-          <div className="cta-buttons">
-            <NavLink to="/signup" className="cta-button">
-              Sign Up
-            </NavLink>
-            <NavLink to="/login" className="cta-button">
-              Login
-            </NavLink>
+    <div>
+      <div className="container-fluid">
+        <div className="nav-container">
+          <header className="nav-header">
+            <div>
+              <a href="/" className="planit-logo">
+                <img src={logo} className="App-logo" alt="logo" />
+              </a>
+            </div>
+            <div>
+              <button type="button" className="btn btn-primary me-3">
+                Login
+              </button>
+              <button type="button" className="btn btn-primary">
+                Sign-Up
+              </button>
+            </div>
+          </header>
+        </div>
+        <div className="container">
+          <div className="p-5 text-center">
+            <div>
+              <img src={logo} className="App-logo-large" alt="logo" />
+            </div>
+            <h2 className="subtitle">
+              Unleash Your Coding Dreams with Collaborative Projects!
+            </h2>
+            <p className="col-lg-8 mx-auto fs-5 text-muted">
+              Welcome to Project Plan-It, the ultimate platform where software
+              engineers, developers, and coders unite to transform passion into
+              reality. Create, collaborate, and conquer coding projects that
+              once seemed too complex to tackle alone. Join our vibrant
+              community and experience the thrill of building together!
+            </p>
+            <div>
+              <button className="browser-buttons" type="button">
+                HACK THE PLANET!
+              </button>
+            </div>
           </div>
-        </section>
-
-        <section className="info-section">
-          {/* Additional Information */}
-          <p>More about your platform, its benefits, and how it works.</p>
-        </section>
-
-        <section className="projects-section">
-          <h2>Current Projects</h2>
-          <div className="projects-grid">
-            {/* Sample card, you can loop this when you have real data */}
-            {[...Array(8)].map((_, index) => (
-              <div key={index} className="project-card">
-                <img src="path_to_project_image" alt="Project title" />
-                <h3>Sample Project {index + 1}</h3>
-                <p>Short project description...</p>
-              </div>
-            ))}
-          </div>
-        </section>
-      </main>
-
-      {/* Optionally add a footer here if needed */}
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default LandingPage;
