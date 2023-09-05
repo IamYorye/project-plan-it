5ec294f397142a3d701599679cc7f5ac245af18f
Gabriel Svetcos <gsvetcos@gmail.com>
2023-09-04 17:28:19 -0700

    redid dashboard and navbar

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -8,51 +8,63 @@ import ProjectList from "./Project/ProjectList";
 import ProjectDetails from "./Project/ProjectDetails";
 import React, { useState, useEffect } from "react";
 import jwtDecode from "jwt-decode";
+import LandingPage from "./Landingpage/LandingPage";
+import Nav from "./navbar";
+import Dashboard from "./Dashboard/Dashboard";
 
 
-function App() {
+function App()
+{
 
 	const [account, setAccount] = useState([])
-	const {token} = useAuthContext()
+	const { token } = useAuthContext()
 
 
 
-	const fetchAccountData = async () => {
-        const accountUrl = `${process.env.REACT_APP_API_HOST}/api/accounts`
-        const fetchConfig = {
-            headers: {
-                "Authorization": `Bearer ${token}`
-            }
-        }
-        try {
-            const response = await fetch(accountUrl, fetchConfig)
-            if(response.ok) {
-                const data = await response.json()
-                setAccount(data)
+	const fetchAccountData = async () =>
+	{
+		const accountUrl = `${process.env.REACT_APP_API_HOST}/api/accounts`
+		const fetchConfig = {
+			headers: {
+				"Authorization": `Bearer ${token}`
+			}
+		}
+		try
+		{
+			const response = await fetch(accountUrl, fetchConfig)
+			if (response.ok)
+			{
+				const data = await response.json()
+				setAccount(data)
 
-                const decodedToken = jwtDecode(token)
+				const decodedToken = jwtDecode(token)
 				console.log(decodedToken)
-            }
-        } catch (error) {
-            console.error("Error fetching account details:", error)
-        }
-    }
+			}
+		} catch (error)
+		{
+			console.error("Error fetching account details:", error)
+		}
+	}
 
-	useEffect(() => {
+	useEffect(() =>
+	{
 		fetchAccountData()
 	}, [])
 
 	return (
 		<div>
 			<BrowserRouter>
+				<Nav />
 				{/* <AuthProvider baseUrl={process.env.REACT_APP_API_HOST}> */}
-					<Routes>
-						<Route path='/signup' element={<SignupForm />}></Route>
-						<Route path='/login' element={<LoginForm />}></Route>
-						<Route path="/projects/new" element={<ProjectForm account={account}/>}></Route>
-						<Route path="/projects" element={<ProjectList account={account}/>}></Route>
-						<Route path="/project-details/:project_id/" element={<ProjectDetails account={account} />}></Route>
-					</Routes>
+				<Routes>
+					<Route path='/' element={<LandingPage />}></Route>
+					<Route path='/dashboard' element={<Dashboard />}></Route>
+					<Route path='/signup' element={<SignupForm />}></Route>
+					<Route path='/login' element={<LoginForm />}></Route>
+					<Route path="/projects/new" element={<ProjectForm account={account} />}></Route>
+					<Route path="/projects" element={<ProjectList account={account} />}></Route>
+					<Route path="/project-details/:project_id/" element={<ProjectDetails account={account} />}></Route>
+				</Routes>
 				{/* </AuthProvider> */}
 			</BrowserRouter>
 		</div>
--- ghi/src/Dashboard/JoinedProjects.js
+++ ghi/src/Dashboard/JoinedProjects.js
@@ -1,87 +1,104 @@
-import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
 
+const statuses = {
+    Complete: 'text-green-700 bg-green-50 ring-green-600/20',
+    'In progress': 'text-gray-600 bg-gray-50 ring-gray-500/10',
+    Archived: 'text-yellow-800 bg-yellow-50 ring-yellow-600/20',
+}
 const projects = [
-	{
-		name: "Graph API",
-		initials: "GA",
-		href: "#",
-		members: 16,
-		bgColor: "bg-pink-600",
-	},
-	{
-		name: "Component Design",
-		initials: "CD",
-		href: "#",
-		members: 12,
-		bgColor: "bg-purple-600",
-	},
-	{
-		name: "Templates",
-		initials: "T",
-		href: "#",
-		members: 16,
-		bgColor: "bg-yellow-500",
-	},
-	{
-		name: "React Components",
-		initials: "RC",
-		href: "#",
-		members: 8,
-		bgColor: "bg-green-500",
-	},
-];
+    {
+        id: 1,
+        name: 'GraphQL API',
+        href: '#',
+        status: 'Complete',
+        createdBy: 'Leslie Alexander',
+        dueDate: 'March 17, 2023',
+        dueDateTime: '2023-03-17T00:00Z',
+    },
+    {
+        id: 2,
+        name: 'New benefits plan',
+        href: '#',
+        status: 'In progress',
+        createdBy: 'Leslie Alexander',
+        dueDate: 'May 5, 2023',
+        dueDateTime: '2023-05-05T00:00Z',
+    },
+    {
+        id: 3,
+        name: 'Onboarding emails',
+        href: '#',
+        status: 'In progress',
+        createdBy: 'Courtney Henry',
+        dueDate: 'May 25, 2023',
+        dueDateTime: '2023-05-25T00:00Z',
+    },
+    {
+        id: 4,
+        name: 'iOS app',
+        href: '#',
+        status: 'In progress',
+        createdBy: 'Leonard Krasner',
+        dueDate: 'June 7, 2023',
+        dueDateTime: '2023-06-07T00:00Z',
+    },
+    {
+        id: 5,
+        name: 'Marketing site redesign',
+        href: '#',
+        status: 'Archived',
+        createdBy: 'Courtney Henry',
+        dueDate: 'June 10, 2023',
+        dueDateTime: '2023-06-10T00:00Z',
+    },
+]
 
-function classNames(...classes) {
-	return classes.filter(Boolean).join(" ");
+function classNames(...classes)
+{
+    return classes.filter(Boolean).join(' ')
 }
 
-export default function JoinedProjects() {
-	return (
-		<div>
-			<h2 className='text-sm font-medium text-gray-500'>Joined Projects</h2>
-			<ul
-				role='list'
-				className='mt-3 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-4'
-			>
-				{projects.map((project) => (
-					<li
-						key={project.name}
-						className='col-span-1 flex rounded-md shadow-sm'
-					>
-						<div
-							className={classNames(
-								project.bgColor,
-								"flex w-16 flex-shrink-0 items-center justify-center rounded-l-md text-sm font-medium text-white"
-							)}
-						>
-							{project.initials}
-						</div>
-						<div className='flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white'>
-							<div className='flex-1 truncate px-4 py-2 text-sm'>
-								<a
-									href={project.href}
-									className='font-medium text-gray-900 hover:text-gray-600'
-								>
-									{project.name}
-								</a>
-								<p className='text-gray-500'>{project.members} Members</p>
-							</div>
-							<div className='flex-shrink-0 pr-2'>
-								<button
-									type='button'
-									className='inline-flex h-8 w-8 items-center justify-center rounded-full bg-transparent bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2'
-								>
-									<span className='sr-only'>Open options</span>
-									<EllipsisVerticalIcon
-										className='h-5 w-5'
-										aria-hidden='true'
-									/>
-								</button>
-							</div>
-						</div>
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+export default function JoinedProjects()
+{
+    return (
+        <ul role="list" className="divide-y divide-gray-100">
+            {projects.map((project) => (
+                <li key={project.id} className="flex items-center justify-between gap-x-6 py-5">
+                    <div className="min-w-0">
+                        <div className="flex items-start gap-x-3">
+                            <p className="text-sm font-semibold leading-6 text-gray-900">{project.name}</p>
+                            <p
+                                className={classNames(
+                                    statuses[project.status],
+                                    'rounded-md whitespace-nowrap mt-0.5 px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset'
+                                )}
+                            >
+                                {project.status}
+                            </p>
+                        </div>
+                        <div className="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
+                            <p className="whitespace-nowrap">
+                                Due on <time dateTime={project.dueDateTime}>{project.dueDate}</time>
+                            </p>
+                            <svg viewBox="0 0 2 2" className="h-0.5 w-0.5 fill-current">
+                                <circle cx={1} cy={1} r={1} />
+                            </svg>
+                            <p className="truncate">Created by {project.createdBy}</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-none items-center gap-x-4">
+                        <a
+                            href={project.href}
+                            className="hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block"
+                        >
+                            View project<span className="sr-only">, {project.name}</span>
+                        </a>
+
+                    </div>
+                </li>
+            ))}
+        </ul>
+    )
 }
