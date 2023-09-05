b3493a2ca081d09c5c4748bb16fb6bdc6918e828
jaspreet <jaspreetsangha204@yahoo.com>
2023-09-05 14:18:39 -0700

    Changed routing

--- ghi/src/Account/LoginForm.js
+++ ghi/src/Account/LoginForm.js
@@ -14,7 +14,7 @@ export default function Login()
         e.preventDefault();
         login(username, password);
         e.target.reset();
-        navigate("/projects/new");
+        navigate("/projects");
     };
     return (
         <>
